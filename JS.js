c8a43b8b3440b3ad39a4256938b6bb944ad6d947
Your Name <you@example.com>
2021-11-10 09:26:24 -0300

    commit

---
+++ JS.js
@@ -0,0 +1,9 @@
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Obrigado por entrar em contato' + nome.value + ' o Cultura MA agradece');
+    }
+
+}
